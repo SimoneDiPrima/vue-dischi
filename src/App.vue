18b8ee7b127177f26b3bf2196beadee37ab27b90
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-07-26 16:14:26 +0200

    creazione layout statico e creazione sass

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,21 @@
 <template>
- <h1 class="text-warning">spotify</h1>
+<div>
+  <header>
+ <i class="fa-brands fa-spotify icon-header"></i>
+ </header>
+ <main>
+  <div class="container">
+    <div class="row">
+      <div class="offset-1 col-10 allDisks">
+
+      </div>
+    </div>
+  </div>
+
+ </main>
+
+</div>
+
 </template>
 
 <script>
@@ -11,5 +27,21 @@ export default {
 </script>
 
 <style lang="scss">
-@import './assets/scss/style.scss'
+@import './assets/scss/style.scss';
+header{
+  background-color: #2E3A46;
+}
+main{
+  height:100vh;
+  background-color: #1E2D3B;
+}
+.allDisks{
+  border:1px solid red;
+  height:100vh;
+}
+.icon-header{
+  font-size: 80px;
+  color:green;
+ margin:10px;
+}
 </style>
